b9ff4fbfa891db4ff7b2b39d36fd69773f662b9c
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-13 22:48:19 +0300

    Make API call for countries data and setup the reducer functions

--- src/features/countries/countriesSlice.js
+++ src/features/countries/countriesSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Action } from '@remix-run/router';
+import { showCountries } from './countriesAction';
 
 const initialState = {
   loading: false,
@@ -6,11 +8,28 @@ const initialState = {
   countryData: [],
   error: false,
   success: false,
-  meaasge: '',
+  message: '',
 };
 const countriesSlice = createSlice({
   name: 'countries',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(showCountries.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showCountries.fulfilled, (state, action) => {
+        state.loading = false;
+        state.countriesData = action.payload;
+        state.success = true;
+      })
+      .addCase(showCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.error = true;
+        state.message = action.payload;
+        state.countriesData = [];
+      });
+  },
 });
 export default countriesSlice.reducer;
